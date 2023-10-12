a355f7537ac17f5bb122af98991e7771d5b778ef
prafuel <official.sonawaneprafull@gmail.com>
2023-10-12 20:19:50 +0530

    working on uploading files, and interaction between frontend and backend

--- web/app.py
+++ web/app.py
@@ -1,9 +1,14 @@
-from flask import Flask,render_template
+from flask import *
 
 app = Flask("__name__")
 
 @app.route("/",method=['GET','POST'])
 def index() :
+
+    if request.method == 'POST':
+        pass
+        # Working HERE.... on UPLOADIN SECTION
+
     return render_template("index.html")
 
 if __name__ == "__main__" :
