5ae523556113ff2e58535c42f81be796f17de9e3
prafuel <official.sonawaneprafull@gmail.com>
2023-10-15 17:22:56 +0530

    ready to host

--- app.py
+++ app.py
@@ -174,6 +174,6 @@ def history():
 
 
 
-if __name__ == "__main__" :
-    # app.run(debug=True, port=8000)
-    app.run(host='0.0.0.0',port=5000)
+# if __name__ == "__main__" :
+#     # app.run(debug=True, port=8000)
+#     app.run(host='0.0.0.0',port=5000)
